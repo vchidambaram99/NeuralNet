771b9640dc83f456fe1f7d6dad18028c6bd595ad
Valliappan Chidambaram <vchidambaram99@gmail.com>
2017-09-28 22:55:20 -0600

    implemented backprop
    next goal is to create some activation functions and some cost functions

--- src/NeuralNets/BasicNeuralNet.hpp
+++ src/NeuralNets/BasicNeuralNet.hpp
@@ -13,18 +13,23 @@
 class BasicNeuralNet{
 public:
 	std::vector<Eigen::MatrixXd> weights;
-	std::vector<Eigen::VectorXd> biases;
+	std::vector<Eigen::MatrixXd> biases;
 	std::vector<Eigen::MatrixXd (*)(Eigen::MatrixXd)> activationFunctions;
 	std::vector<Eigen::MatrixXd (*)(Eigen::MatrixXd)> derivatives;
+	Eigen::MatrixXd (*costFunction)(Eigen::MatrixXd);
 	bool bias;
 	BasicNeuralNet(){
+		costFunction = nullptr;
 		bias = true;
 	}
-	BasicNeuralNet(std::vector<int> layerSizes,std::vector<Eigen::MatrixXd (*)(Eigen::MatrixXd)> _activationFunctions, std::vector<Eigen::MatrixXd (*)(Eigen::MatrixXd)> _derivatives ,bool _bias){
+	BasicNeuralNet(std::vector<int> layerSizes, std::vector<Eigen::MatrixXd (*)(Eigen::MatrixXd)> _activationFunctions,
+				   std::vector<Eigen::MatrixXd (*)(Eigen::MatrixXd)> _derivatives,
+				   Eigen::MatrixXd (*_costFunction) (Eigen::MatrixXd,Eigen::MatrixXd), bool _bias){
 		//layerSizes is a vector of the size of each layer
 		//activationFunctions is a vector to function pointers that apply to MatrixXds
 		//derivatives is the derivatives of activationFunctions
 		bias = _bias;
+		costFunction = _costFunction;
 		weights = std::vector(layerSizes.size()-1);
 		std::vector<Eigen::MatrixXd (*)(Eigen::MatrixXd)> activationFunctions = _activationFunctions;
 		std::vector<Eigen::MatrixXd (*)(Eigen::MatrixXd)> derivatives = _derivatives;
@@ -38,7 +43,7 @@ public:
 			}
 		}
 	}
-	Eigen::VectorXd fire(Eigen::VectorXd input){
+	Eigen::VectorXd fire(Eigen::MatrixXd input){//columns of input are each case (can do multiple cases simultaneously)
 		for(int i = 0;i<weights.size();i++){
 			if(bias){
 				input = (*activationFunctions[i])((weights[i]*input)+biases[i]);
@@ -48,8 +53,31 @@ public:
 		}
 		return input;
 	}
-	Eigen::VectorXd backprop(){
-
+	Eigen::MatrixXd backprop(Eigen::MatrixXd input, Eigen::MatrixXd answer){
+		std::vector<Eigen::MatrixXd> activations = std::vector<Eigen::MatrixXd>(weights.size()+1);//activations of all (even input) layers
+		std::vector<Eigen::MatrixXd> derivatives = std::vector<Eigen::MatrixXd>(weights.size());  //derivatives of all non-input layers
+		std::vector<Eigen::MatrixXd> errors 	 = std::vector<Eigen::MatrixXd>(weights.size());  //errors of all non-input layers
+		activations[0] = &input;
+		for(int i = 0;i<weights.size();i++){
+			Eigen::MatrixXd weighted;
+			if(bias)weighted = (weights[i]*activations[i])+biases[i];
+			else weighted = weights[i]*activations[i];
+			activations[i+1] = (*activationFunctions[i])(weighted);
+			derivatives[i] = (*derivatives[i])(weighted);
+		}
+		errors.back() = costFunction(activations.back(),answer).cwiseProduct(derivatives.back()); //calculates error of last layer
+		for(int i = errors.size()-2;i>=0;i--){
+			errors[i] = (weights[i+1].transpose()*errors[i+1]).cwiseProduct(derivatives[i]);
+		}
+		if(bias){
+			for(int i = 0;i<biases.size();i++){
+				biases[i]+=errors[i];
+			}
+		}
+		for(int i = 0;i<weights.size();i++){
+			weights[i]+=errors[i]*activations[i].transpose();
+		}
+		return activations.back();
 	}
 };
 
